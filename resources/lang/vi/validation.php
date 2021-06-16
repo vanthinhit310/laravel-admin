bc5c34466b2125853da88b58254e921fbc812f6f
Lê Văn Thịnh <vanthinhit310@gmail.com>
2021-06-16 22:15:22 +0700

    -

---
+++ resources/lang/vi/validation.php
@@ -0,0 +1,127 @@
+<?php
+
+return array (
+  'accepted' => ' :attribute phải được chấp nhận.',
+  'active_url' => ' :attribute không phải là một URL hợp lệ.',
+  'after' => ' :attribute phải là một ngày sau :date.',
+  'after_or_equal' => ' :attribute phải là một ngày sau hoặc bằng :date.',
+  'alpha' => ' :attribute phải chỉ chứa các chữ cái.',
+  'alpha_dash' => ' :attribute chỉ phải chứa các chữ cái, số, dấu gạch ngang và dấu gạch dưới.',
+  'alpha_num' => ' :attribute chỉ phải chứa các chữ cái và số.',
+  'array' => ' :attribute phải là một mảng.',
+  'before' => ' :attribute phải là một ngày trước :date.',
+  'before_or_equal' => ' :attribute phải là một ngày trước hoặc bằng :date.',
+  'between' => 
+  array (
+    'numeric' => ' :attribute phải nằm giữa :min và :max.',
+    'file' => ' :attribute phải nằm giữa :min và :max kilobyte.',
+    'string' => ' :attribute phải nằm giữa các ký tự :min và :max.',
+    'array' => ' :attribute phải có giữa các mặt hàng :min và :max.',
+  ),
+  'boolean' => 'Trường :attribute phải đúng hoặc sai.',
+  'confirmed' => 'Xác nhận :attribute không khớp.',
+  'date' => ' :attribute không phải là một ngày hợp lệ.',
+  'date_equals' => ' :attribute phải là một ngày bằng :date.',
+  'date_format' => ' :attribute không khớp với định dạng :format.',
+  'different' => ' :attribute và :other phải khác nhau.',
+  'digits' => ' :attribute phải là chữ số :digits.',
+  'digits_between' => ' :attribute phải nằm giữa các chữ số :min và :max.',
+  'dimensions' => ' :attribute có kích thước hình ảnh không hợp lệ.',
+  'distinct' => 'Trường :attribute có một giá trị trùng lặp.',
+  'email' => ' :attribute phải là một địa chỉ email hợp lệ.',
+  'ends_with' => ' :attribute phải kết thúc với một trong những điều sau đây: :values.',
+  'exists' => ' :attribute đã chọn không hợp lệ.',
+  'file' => ' :attribute phải là một tập tin.',
+  'filled' => 'Trường :attribute phải có một giá trị.',
+  'gt' => 
+  array (
+    'numeric' => ' :attribute phải lớn hơn :value.',
+    'file' => ' :attribute phải lớn hơn :value kilobyte.',
+    'string' => ' :attribute phải lớn hơn các ký tự :value.',
+    'array' => ' :attribute phải có nhiều hơn :value.',
+  ),
+  'gte' => 
+  array (
+    'numeric' => ' :attribute phải lớn hơn hoặc bằng :value.',
+    'file' => ' :attribute phải lớn hơn hoặc bằng :value kilobyte.',
+    'string' => ' :attribute phải lớn hơn hoặc bằng :value ký tự.',
+    'array' => ' :attribute phải có các mặt hàng :value trở lên.',
+  ),
+  'image' => ' :attribute phải là một hình ảnh.',
+  'in' => ' :attribute đã chọn không hợp lệ.',
+  'in_array' => 'Trường :attribute không tồn tại trong :other.',
+  'integer' => ' :attribute phải là một số nguyên.',
+  'ip' => ' :attribute phải là một địa chỉ IP hợp lệ.',
+  'ipv4' => ' :attribute phải là một địa chỉ IPv4 hợp lệ.',
+  'ipv6' => ' :attribute phải là một địa chỉ IPv6 hợp lệ.',
+  'json' => ' :attribute phải là một chuỗi JSON hợp lệ.',
+  'lt' => 
+  array (
+    'numeric' => ' :attribute phải nhỏ hơn :value.',
+    'file' => ' :attribute phải nhỏ hơn :value kilobyte.',
+    'string' => ' :attribute phải nhỏ hơn các ký tự :value.',
+    'array' => ' :attribute phải có ít hơn các mặt hàng :value.',
+  ),
+  'lte' => 
+  array (
+    'numeric' => ' :attribute phải nhỏ hơn hoặc bằng :value.',
+    'file' => ' :attribute phải nhỏ hơn hoặc bằng :value kilobyte.',
+    'string' => ' :attribute phải nhỏ hơn hoặc bằng :value ký tự.',
+    'array' => ' :attribute không được có nhiều hơn các mặt hàng :value.',
+  ),
+  'max' => 
+  array (
+    'numeric' => ' :attribute không được lớn hơn :max.',
+    'file' => ' :attribute không được lớn hơn :max kilobyte.',
+    'string' => ' :attribute không được lớn hơn các ký tự :max.',
+    'array' => ' :attribute không được có nhiều hơn các mặt hàng :max.',
+  ),
+  'mimes' => ' :attribute phải là một tệp loại: :values.',
+  'mimetypes' => ' :attribute phải là một tệp loại: :values.',
+  'min' => 
+  array (
+    'numeric' => ' :attribute phải có ít nhất :min.',
+    'file' => ' :attribute phải có ít nhất :min kilobyte.',
+    'string' => ' :attribute phải có ít nhất các ký tự :min.',
+    'array' => ' :attribute phải có ít nhất các mặt hàng :min.',
+  ),
+  'multiple_of' => ' :attribute phải là bội số của :value.',
+  'not_in' => ' :attribute đã chọn không hợp lệ.',
+  'not_regex' => 'Định dạng :attribute không hợp lệ.',
+  'numeric' => ' :attribute phải là một số.',
+  'password' => 'Mật khẩu không đúng.',
+  'present' => 'Trường :attribute phải có mặt.',
+  'regex' => 'Định dạng :attribute không hợp lệ.',
+  'required' => 'Trường :attribute là bắt buộc.',
+  'required_if' => 'Trường :attribute được yêu cầu khi :other là :value.',
+  'required_unless' => 'Trường :attribute là bắt buộc trừ khi :other ở :values.',
+  'required_with' => 'Trường :attribute là bắt buộc khi :values có mặt.',
+  'required_with_all' => 'Trường :attribute là bắt buộc khi :values có mặt.',
+  'required_without' => 'Trường :attribute là bắt buộc khi không có :values.',
+  'required_without_all' => 'Trường :attribute được yêu cầu khi không có :values.',
+  'prohibited' => 'Trường :attribute bị cấm.',
+  'prohibited_if' => 'Trường :attribute bị cấm khi :other là :value.',
+  'prohibited_unless' => 'Trường :attribute bị cấm trừ khi :other ở :values.',
+  'same' => ' :attribute và :other phải khớp.',
+  'size' => 
+  array (
+    'numeric' => ' :attribute phải là :size.',
+    'file' => ' :attribute phải là :size kilobyte.',
+    'string' => ' :attribute phải là ký tự :size.',
+    'array' => ' :attribute phải chứa các mặt hàng :size.',
+  ),
+  'starts_with' => ' :attribute phải bắt đầu với một trong những thao tác sau: :values.',
+  'string' => ' :attribute phải là một chuỗi.',
+  'timezone' => ' :attribute phải là một khu vực hợp lệ.',
+  'unique' => ' :attribute đã được thực hiện.',
+  'uploaded' => ' :attribute không tải lên.',
+  'url' => 'Định dạng :attribute không hợp lệ.',
+  'uuid' => ' :attribute phải là một UUID hợp lệ.',
+  'custom' => 
+  array (
+    'attribute-name' => 
+    array (
+      'rule-name' => 'thông báo tùy chỉnh',
+    ),
+  ),
+);
